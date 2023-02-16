1f9c2986bc96d1796359c6517a5528456dde4d1e
russell <1406409859@qq.com>
2023-02-16 23:39:45 +0800

    添加smb服务器地址，保存在本地配置中，不作为上传数据，做到数据隔离

---
+++ app/src/main/java/com/russell/androidsmb/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.russell.androidsmb
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private  val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: "+BuildConfig.BUILD_TYPE)
+    }
+
+}
